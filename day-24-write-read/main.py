bda857067b96c172a7fec94db7b90e5f4cad521a
Ella Jozinovic <ellajozi01@gmail.com>
2026-02-15 19:06:04 +1030

    Rename for clarity

--- day-24-write-read/main.py
+++ day-24-write-read/main.py
@@ -6,7 +6,7 @@ file.close()
 
 # Different way to open
 # Absolute file path
-with open("/home/sleepy/PycharmProjects/day-16-100/day-24/my_file.txt") as file:
+with open("/day-24-write-read/my_file.txt") as file:
     contents = file.read()
     print(contents)
     # Don't need to manually close
